6477c3e00fae377a175f1e63acb883c075ed6364
Alexandre Quessy <alexandre@quessy.net>
2019-06-19 17:24:42 -0400

    Add email to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,46 +13,50 @@ import { FormattedMessage } from 'react-intl';
 const Footer = (props) => (
   <div
     style={{
-      padding: '80px 0 0 0',
+      margin: 'auto',
+      width: '50%',
       marginTop: '1.45rem',
-      background: '#53535',
+      maxWidth: 1280,
+      padding: '1.45rem 1.0875rem',
     }}
   >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <span
-        style={{
-          marginLeft: '10px',
-          marginRight: '10px',
-          verticalAlign: 'baseline',
-          lineHeight: '50px',
-        }}
-      >
+    <div className="columns">
+
+      <div className="column">
         &copy; 2019 <FormattedMessage id="ferrettiLab" />
-      </span>
-      <span>
-        <span style={{ marginLeft: '10px', marginRight: '10px' }}>
-          Dr. Vincent Ferretti &nbsp;
-          <a href="https://recherche.chusj.org" target="blank">
-           <FormattedMessage id="crsj" />
-          </a>
-          <br />
-          <span style={{ marginLeft: '10px', marginRight: '10px' }}>
-           <FormattedMessage id="addresseChuSteJustine" />
-          </span>
-        </span>
-      </span>
-      <GitHubLink
-        githubLink="https://github.com/cr-ste-justine/"
-        style={{ lineHeight: '50px', verticalAlign: 'baseline' }}
-      />
-      &nbsp;
-      <a href="tel:514-345-4931,4926">514-345-4931 #4926</a>
+      </div>
+
+      <div className="column">
+        Dr. Vincent Ferretti
+        <br />
+        <a href="tel:514-345-4931,4926" style={{ verticalAlign: 'top' }}>514-345-4931 #4926</a>
+      </div>
+
+      <div className="column">
+        <a href="https://recherche.chusj.org" target="blank">
+         <FormattedMessage id="crsj" />
+        </a>
+        <br />
+        <FormattedMessage id="addresseChuSteJustine" />
+      </div>
+
+    </div>
+
+    <div className="columns">
+
+      <div className="column">
+      </div>
+
+      <div className="column">
+        <GitHubLink
+          githubLink="https://github.com/cr-ste-justine/"
+          style={{ lineHeight: '50px', verticalAlign: 'baseline' }}
+        />
+      </div>
+
+      <div className="column">
+        <img src="/images/email-image.png" />
+      </div>
     </div>
   </div>
 );
