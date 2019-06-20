b8783cc6015f4a8dd00d6452e14663e9dbe82562
Alexandre Quessy <alexandre@quessy.net>
2019-06-20 17:51:54 -0400

    Add project images - but they look not so good

--- src/pages/projects.en.js
+++ src/pages/projects.en.js
@@ -4,15 +4,16 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../layouts/en';
+import '../style/style.scss';
 
 const ProjectsPage = (props) => (
   <Layout location={props.location}>
-    <div>
+    <div className="projectsPage">
       <h1>What do we do?</h1>
       <h2>Current projects</h2>
       <div>
         <h3>NIH Gabriella Miller Kids First Data Resource Center</h3>
-        <img src="/images/project_kidsfirst.png" />
+        <img src="/images/project_kidsfirst.png" width="320" />
         <p>
           The Kids First Data Resource Center (DRC) is a collaborative pediatric
           research effort created to accelerate data-driven discoveries and the
@@ -27,18 +28,21 @@ const ProjectsPage = (props) => (
 
       <div>
         <h3>Centre québécois de données génomiques</h3>
+        <img src="/images/project_CQDG.png" width="320" />
         <p>
-          Le centre québécois de données génomiques (CQDG) vise à offrir une
-          infrastructure infonuagique robuste et sécuritaire pour l’hébergement
-          et l’harmonisation des données génomiques générées par les activités
-          cliniques et de recherche en génomique au Québec.
+          The Centre québécois de données génomiques is a unique 
+          collaborative research infrastructure in Canada whose mission is to 
+          enable precision medicine by promoting the harmonization and sharing 
+          of genomics and phenotype data produced by clinical and research 
+          activities in Québec. 
         </p>
       </div>
 
       <div>
         <h3>Centre québécois de génomique clinique</h3>
+        <img src="/images/project_clin.png" width="320" />
         <p>
-          Le centre québécois de génomique clinique (CQGC) vise à offrir une
+          The Centre québécois de génomique clinique (CQGC) vise à offrir une
           infrastructure infonuagique robuste et sécuritaire pour l’hébergement
           et l’harmonisation des données génomiques générées par les activités
           de recherche en génomique au Québec.
@@ -48,10 +52,30 @@ const ProjectsPage = (props) => (
       <hr />
       
       <h2>Former projects</h2>
-      <ul>
-        <li><a href="https://dcc.icgc.org/">The ICGC Data Portal</a></li>
-        <li><a href="https://cancercollaboratory.org/">The Cancer Genome Collaboratory</a></li>
-      </ul>
+
+      <div>
+        <h3>The ICGC Data Portal</h3>
+        <img src="/images/project_ICGC.png" width="320" />
+        <p>
+          <a href="https://dcc.icgc.org/">dcc.icgc.org</a>
+        </p>
+      </div>
+
+      <div>
+        <h3>The Cancer Genome Collaboratory</h3>
+        <img src="/images/project_cancercollaboratory.png" width="320" />
+        <p>
+          <a href="https://cancercollaboratory.org/">cancercollaboratory.org</a>
+        </p>
+      </div>
+
+      <div>
+        <h3>Genomic Data Commons Data Portal</h3>
+        <img src="/images/project_GDC.png" width="320" />
+        <p>
+          <a href="https://portal.gdc.cancer.gov/">portal.gdc.cancer.gov</a>
+        </p>
+      </div>
         
     </div>
   </Layout>
--- src/pages/projects.fr.js
+++ src/pages/projects.fr.js
@@ -4,15 +4,16 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../layouts/fr';
+import '../style/style.scss';
 
 const ProjectsPage = (props) => (
   <Layout location={props.location}>
-    <div>
+    <div className="projectsPage">
       <h1>Sur quoi travaillons-nous&nbsp;?</h1>
       <h2>Projets en cours</h2>
       <div>
         <h3>NIH Gabriella Miller Kids First Data Resource Center</h3>
-        <img src="/images/project_kidsfirst.png" />
+        <img src="/images/project_kidsfirst.png" width="320" />
         <p>
           The Kids First Data Resource Center (DRC) is a collaborative pediatric
           research effort created to accelerate data-driven discoveries and the
@@ -26,6 +27,7 @@ const ProjectsPage = (props) => (
 
       <div>
         <h3>Centre québécois de données génomiques</h3>
+        <img src="/images/project_CQDG.png" width="320" />
         <p>
           Le centre québécois de données génomiques (CQDG) vise à offrir une
           infrastructure infonuagique robuste et sécuritaire pour l’hébergement
@@ -36,6 +38,7 @@ const ProjectsPage = (props) => (
 
       <div>
         <h3>Centre québécois de génomique clinique</h3>
+        <img src="/images/project_clin.png" width="320" />
         <p>
           Le centre québécois de génomique clinique (CQGC) vise à offrir une
           infrastructure infonuagique robuste et sécuritaire pour l’hébergement
@@ -47,10 +50,30 @@ const ProjectsPage = (props) => (
       <hr />
       
       <h2>Anciens projets</h2>
-      <ul>
-        <li><a href="https://dcc.icgc.org/">The ICGC Data Portal</a></li>
-        <li><a href="https://cancercollaboratory.org/">The Cancer Genome Collaboratory</a></li>
-      </ul>
+
+      <div>
+        <h3>The ICGC Data Portal</h3>
+        <img src="/images/project_ICGC.png" width="320" />
+        <p>
+          <a href="https://dcc.icgc.org/">dcc.icgc.org</a>
+        </p>
+      </div>
+
+      <div>
+        <h3>The Cancer Genome Collaboratory</h3>
+        <img src="/images/project_cancercollaboratory.png" width="320" />
+        <p>
+          <a href="https://cancercollaboratory.org/">cancercollaboratory.org</a>
+        </p>
+      </div>
+
+      <div>
+        <h3>Genomic Data Commons Data Portal</h3>
+        <img src="/images/project_GDC.png" width="320" />
+        <p>
+          <a href="https://portal.gdc.cancer.gov/">portal.gdc.cancer.gov</a>
+        </p>
+      </div>
 
     </div>
   </Layout>
