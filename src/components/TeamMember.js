2968cac1fb65f5478d8e8b1fd1bf97df2bb0a2a4
Alexandre Quessy <alexandre@quessy.net>
2019-06-21 15:47:51 -0400

    Fix padding between team member cards

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,5 +1,5 @@
 /**
- * @file The GitHubLink component.
+ * @file The TeamMember component.
  */
 import React from 'react';
 import PropTypes from 'prop-types';
@@ -7,48 +7,50 @@ import { FaGithub } from 'react-icons/fa';
 import '../style/style.scss';
 
 /**
- * Component for showing a link a GitHub team.
+ * Component for showing the info about a person in the team.
  *
  * @todo For some reason, there is no gap between each column.
  * Perhaps the SASS variables for Bulma are not used?
  */
 const TeamMember = (props) => (
-  <div className="box column is-half">
-    <article className="media">
-      <div className="media-left"></div>
-      <div className="media-content">
-        <div className="content">
-          <p>
-            <strong>{props.fullName}</strong> &nbsp;
-          </p>
-          <p>
-            <a href={'https://github.com/' + props.githubUser}>
-              <small>@{props.githubUser}</small>
-            </a>
-          </p>
-          <p>{props.title}</p>
-        </div>
-        <nav className="level is-mobile">
-          <div className="level-left">
-            <a
-              className="level-item"
-              aria-label="reply"
-              href={'https://github.com/' + props.githubUser}
-            >
-              <span className="icon is-small">
-                <FaGithub size="fa-2x" />
-              </span>
-            </a>
+  <div className="column is-half">
+    <div className="box" style={{padding: '5px'}}>
+      <article className="media">
+        <div className="media-left"></div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{props.fullName}</strong> &nbsp;
+            </p>
+            <p>
+              <a href={'https://github.com/' + props.githubUser}>
+                <small>@{props.githubUser}</small>
+              </a>
+            </p>
+            <p>{props.title}</p>
           </div>
-        </nav>
-      </div>
-    </article>
+          <nav className="level is-mobile">
+            <div className="level-left">
+              <a
+                className="level-item"
+                aria-label="reply"
+                href={'https://github.com/' + props.githubUser}
+              >
+                <span className="icon is-small">
+                  <FaGithub size="fa-2x" />
+                </span>
+              </a>
+            </div>
+          </nav>
+        </div>
+      </article>
+    </div>
   </div>
 );
 
 TeamMember.propTypes = {
   /**
-   * Prototype of prop "githubUser"
+   * Prototype of prop "TeamMember"
    */
   githubUser: PropTypes.string,
   fullName: PropTypes.string,
